a8962d9b97b5eab327d206f202d627b887e105b3
Gladturtle <akashsam88@gmail.com>
2025-03-16 10:05:01 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,26 +1,22 @@
 import tkinter as tk
 import time 
 import psutil
+from backend import detection
 
-def detection():
-    while True:
-        running = "Code.exe" in [p.name() for p in psutil.process_iter()]
-        
-        return running
-print(detection())
+while detection()
 
-root = tk.Tk()
-root.overrideredirect(True) 
-root.geometry("400x300")  
+    root = tk.Tk()
+    root.overrideredirect(True) 
+    root.geometry("400x300")  
 
 
-srs_cat = tk.PhotoImage(file='srs_cat.png')
+    srs_cat = tk.PhotoImage(file='srs_cat.png')
 
-srs_cat_label = tk.Label(root,image=srs_cat)
-srs_cat_label.pack()
+    srs_cat_label = tk.Label(root,image=srs_cat)
+    srs_cat_label.pack()
 
 
 
 
 
-root.mainloop()
+    root.mainloop()
