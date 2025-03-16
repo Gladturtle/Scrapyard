54dd4592639a917a7aa5c6de2b064a124f11541a
Gladturtle <akashsam88@gmail.com>
2025-03-16 09:24:10 +0530

    the code now cat

--- main.py
+++ main.py
@@ -6,19 +6,15 @@ def on_drag(event):
 root = tk.Tk()
 root.overrideredirect(True) 
 root.geometry("400x300")  
-root.configure(bg="lightblue")  
 
 
-title_bar = tk.Frame(root, bg="blue", relief="raised", bd=2, height=30)
-title_bar.pack(fill=tk.X)
-title_bar.bind("<B1-Motion>", on_drag)
+srs_cat = tk.PhotoImage(file='srs_cat.png')
+
+srs_cat_label = tk.Label(root,image=srs_cat)
+srs_cat_label.pack()
 
 
-close_btn = tk.Button(title_bar, text="X", bg="red", fg="white", command=root.destroy)
-close_btn.pack(side=tk.RIGHT, padx=5, pady=2)
 
 
-label = tk.Label(root, text="Borderless Tkinter Window", bg="lightblue", font=("Arial", 14))
-label.pack(pady=50)
 
 root.mainloop()
